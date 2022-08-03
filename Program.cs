2fa1a4064e472f10d7ca85a614f8b912c1f7d8dc
sandinm62 <82060084+sandinm62@users.noreply.github.com>
2022-08-03 12:38:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    //Сандин Михаил
+    // Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ)
+    // по формуле I=m/(h* h); где m — масса тела в килограммах, h — рост в метрах.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mass, growth, i;
+            Console.Write("Enter you mass (Kg): ");
+            mass = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter you growth (m): ");
+            growth = Convert.ToDouble(Console.ReadLine());
+            i = mass / (growth * growth);
+            Console.Clear();
+            Console.WriteLine("You body mass index: " + "{0:F}", i); //выводим индекс массы тела и округляем колличество знаков после запятой до двух символо
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
